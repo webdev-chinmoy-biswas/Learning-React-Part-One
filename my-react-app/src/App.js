62d35c622d9bd0e646db1f71fcca9ccec270598e
webdev-chinmoy-biswas <chinmoy709@gmail.com>
2022-04-04 15:40:11 +0600

    added component

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,12 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+const userName = ["aiman", "sadik", "minhaz", "tamim"]
+
 function App() {
   return (
     <div className="App">
-      <h1>First React App</h1>
+      {
+        userName.map(name => <BoxCard name={name}></BoxCard>)
+      }
     </div>
-  );
+  )
 }
+
+ function BoxCard(props) {
+  console.log(props)
+ return (
+   <div className="card-design">
+       <h1>{props.name}</h1>
+       <h3>Age: {props.age}</h3>
+      <h5>Gender: {props.gender}</h5>
+      <p>Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Porro, alias.</p>
+    </div>
+   )
+ }
 
 export default App;
